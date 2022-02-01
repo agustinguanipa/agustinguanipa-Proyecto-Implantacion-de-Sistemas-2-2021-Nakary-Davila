3d50c53ee4f31ce23f559e2029063d42055baa64
Agustin Guanipa <agustin_guanipa@hotmail.com>
2022-01-31 23:03:00 -0400

    Inicio Proyecto, correciones en los estilos y colores

---
+++ constant.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('SITE_NAME','La Magica Kendy - Administracion');
+define('FRONT_SITE_NAME','La Magica Kendy');
+
+define('FRONT_SITE_PATH','http://localhost:8888/is_nakarydavila_22021/');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/is_nakarydavila_22021/");
+
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+define('SITE_DISH_IMAGE',FRONT_SITE_PATH."media/dish/");
+
+define('SERVER_BANNER_IMAGE',SERVER_IMAGE."media/banner/");
+define('SITE_BANNER_IMAGE',FRONT_SITE_PATH."media/banner/");
+
+?>
---
+++ database.inc.php
@@ -0,0 +1,5 @@
+
+<?php
+date_default_timezone_set('America/Caracas');
+$con=mysqli_connect('localhost','root','root','lmkdb');
+?>
